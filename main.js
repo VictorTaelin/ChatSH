62e0cc5d8c6689e9f601723b419c3a873a06b050
Victor Taelin <victor.taelin@gmail.com>
2024-02-11 19:28:20 -0300

    update model

--- main.js
+++ main.js
@@ -69,10 +69,6 @@ The user system and shell version are:
 
 ${SHELL}
 
-In addition, the following commands are available:
-${await is_installed("kind2") ? "\n- kind2 check file.kind2: type-checks a file using the Kind-Lang" : ""}
-${await is_installed("ag") ? "\n- ag: the silver searcher. when using, show only file names." : ""}
-
 Guidelines:
 
 When asked to write or modify a file, create a sh command to write that file,
@@ -87,40 +83,19 @@ echo "$poem" > cat.txt
 
 When asked to query an API, you will write a sh command to make the request.
 
-When searching on file names, prefer 'find'; on contents, perfer 'ag'.
-
 Always assume commands are installed. Never write commands to install things.
-
-${await is_installed("kind2") ?
-`When the user asks to write a init file in Kind, write a single file with just 2
-lines: the type and one equation with a hole. Do not include other definitions
-such as types: these already exist on Kindex.
-` : ""}
-
-${await is_installed("kind2") ?
-`When asked to create a new Kind file, write just a type and 1 clause. Example:
-
-Main (b: Bool) : Equal Bool (Bool.not (Bool.not b)) b
-Main b = ?hole_0
-
-Do not include types or other definitions, they already exist on Kindex.
-
-Note: Kind is a language similar to Agda. Examples:
-
-${get_kind_examples()}
-` : ""}
 `;
 
 // initialize ChatGPT API with your API key
 const api = new ChatGPTAPI({
   apiKey: TOKEN,
-  maxModelTokens: 8096,
+  maxModelTokens: 128000,
   systemMessage: SYSTEM,
   completionParams: {
     model: MODEL,
     stream: true,
     temperature: 0.5,
-    max_tokens: 1024,
+    max_tokens: 4096,
   }
 });
 
@@ -211,97 +186,12 @@ async function get_token() {
 }
 
 async function get_model() {
-  return "gpt-4";
+  return "gpt-4-0125-preview";
 }
 
 async function get_shell() {
   const shellInfo = (await execAsync('uname -a && $SHELL --version')).stdout.trim();
   return shellInfo;
 }
-
-function get_kind_examples() {
-  return `
-type Bool {
-  true
-  false
-}
-
-type List (t) {
-  nil
-  cons (head: t) (tail: List t)
-}
-
-record Sigma (a: Type) (f: a -> Type) {
-  fst : a
-  snd : f fst
-}
-
-type Vector <a: Type> ~ (len: Nat) {
-  nil : Vector a Nat.zero
-  cons (len: Nat) (head: a) (tail: Vector a len) : Vector a (Nat.succ len)
-}
-
-Bool.not (b: Bool) : Bool
-Bool.not Bool.true  = Bool.false
-Bool.not Bool.false = Bool.true
-
-List.head <a> (xs: List a) : Maybe (List a)
-List.head List.nil              = Maybe.none
-List.head (List.cons head tail) = Maybe.some head
-
-// Or, using match:
-List.head <a> (xs: List a) : Maybe (List a) {
-  match List xs {
-    nil => Maybe.none
-    cons => Maybe.some xs.head
-  }
-}
-
-List.fold <a> <p> (xs: List a) : p -> (a -> p -> p) -> p
-List.fold List.nil              = nil => cons => nil
-List.fold (List.cons head tail) = nil => cons => cons head ((List.fold tail) nil cons)
-
-Sigma.snd <a> <b: a -> Type> (s: Sigma a b) : b (Sigma.fst a b s)
-Sigma.snd (Sigma.new x y) = y
-
-Vector.create <a> (len: Nat) (f: Nat -> a) : Vector a len
-Vector.create Nat.zero     f = Vector.nil
-Vector.create (Nat.succ p) f =
-  let head = f Nat.zero
-  let tail = Vector.create p (x => f (Nat.succ x))
-  Vector.cons p head tail
-
-Main : IO Unit {
-  do IO {
-    ask name = IO.prompt "your name?"
-    IO.print (String.join "" ["Hi " name])
-  }
-}
-
-// Proof: a == a + 0
-Nat.add.comm.zero (a: Nat) : Equal Nat a (Nat.add a Nat.zero)
-// goal: 0 == 0
-Nat.add.comm.zero Nat.zero =
-  // 0 == 0
-  let ret = Equal.refl
-  ret
-// goal: S a == S (a + 0)
-Nat.add.comm.zero (Nat.succ a) =
-  // a == a + 0
-  let ind = Nat.add.comm.zero a
-  // S a == S (a + 0)
-  let app = Equal.apply (x => Nat.succ x) ind
-  app
-
-// Proof: (S a) != 0
-Nat.succ_not_zero (a: Nat) (e: Equal Nat (Nat.succ a) Nat.zero) : Empty
-Nat.succ_not_zero a e =
-  // false == true
-  let app = Equal.apply (x => Nat.is_zero x) e
-  // empty
-  let emp = Bool.false_not_true app
-  emp
-`;
-};
 
 main();
